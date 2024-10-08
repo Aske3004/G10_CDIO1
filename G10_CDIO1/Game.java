ac4f624779893816c71c033be4a220591e5a5aac
Kasper S. Kofod <59373545+kappersk1764@users.noreply.github.com>
2024-10-08 18:09:01 +0200

    Update Game.java

--- G10_CDIO1/Game.java
+++ G10_CDIO1/Game.java
@@ -18,6 +18,29 @@ class Game {
         Die player1Die2 = new Die("player1Die2");
         Die player2Die1 = new Die("player2Die1");
         Die player2Die2 = new Die("player2Die2");
-        
+
+        while (!player1Won && !player2Won){
+            System.out.println("The scoreboard is currently:");
+            System.out.println("Player1: " + player1Point);
+            System.out.println("Player2: " + player2Point);
+            System.out.println("Press Enter to continue with " + playerTurn + "'s turn");
+            try{scanner.nextLine();}
+            catch(Exception e){}  
+            
+            if(playerTurn.equals("Player1")){
+                player1Die1.rollDie();
+                player1Die2.rollDie();
+                player1Point += (player1Die1.dieValue + player1Die2.dieValue);
+                System.out.println("Player1 just rolled a " + player1Die1.dieValue + " and a " + player1Die2.dieValue);
+                if((player1Die1.dieValue == player1Die2.dieValue)){
+                    playerTurn = "Player1";
+                    System.out.println("Player1 has rolled the same value on both dice and therefore gets another turn");
+                }
+                else{
+                    playerTurn = "Player2";
+                }
+                
+            }
+            }       
+        }
     }
-}
